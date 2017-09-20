92a7101c44f69b5b1b415a49c26eb54503864e42
debobrotodasrobin <debobrotodasrobin@gmail.com>
2017-09-20 15:41:29 -0400

    New task adition completed

--- web_app_example/app_first.py
+++ web_app_example/app_first.py
@@ -1,4 +1,5 @@
-from bottle import route, run, template, request
+from bottle import route, run,debug, template, request
+import sqlite3
 
 @route('/hello/<name>')
 def index(name):
@@ -14,4 +15,32 @@ def index(name):
 
     return template('{{tag1}}Hello {{name}}{{tag2}}!', name=name,tag1=tag1,tag2 = tag2)
 
-run(host='localhost', port=8080)
+@route('/new', method='GET')
+def new_item():
+
+    print("gfdksg")
+    new = request.GET.task.strip()
+
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
+    new_id = c.lastrowid
+
+    conn.commit()
+    c.close()
+    print("Done")
+
+    return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+
+@route('/todo')
+def todo_list():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
+    result = c.fetchall()
+    c.close()
+    output = template('make_table', rows=result)
+    return output
+
+run(host='localhost', port=8080, reloader=True)
